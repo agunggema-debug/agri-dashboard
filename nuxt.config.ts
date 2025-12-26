69446838d6cd0bb7dd8bb36ecd19c37db9524482
Your Name <agunggema@gmail.com>
2025-12-26 15:11:01 +0700

    Fix nuxt config: remove target and update assets dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-12-26",
   modules: ["@nuxtjs/tailwindcss"],
   ssr: false, // Menghindari masalah hydration saat dev
-  target: "static", // Memberitahu Nuxt untuk mode statis
   app: {
     baseURL: "/agri-dashboard/", // Sesuaikan dengan nama repository GitHub
+    buildAssetsDir: "assets",
   },
+
   experimental: {
     payloadExtraction: false,
   },
+
+  compatibilityDate: "2024-04-09",
 });
