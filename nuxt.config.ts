61044c7b62ccc17b49c7ef2883a4c3aa54557c6c
Your Name <agunggema@gmail.com>
2025-12-26 14:06:20 +0700

    Initial commit dashboard

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   ssr: false, // Penting untuk GitHub Pages
   app: {
-    baseURL: "https://github.com/agunggema-debug/agri-Dashboard.git", // Sesuaikan dengan nama repository GitHub
+    baseURL: "https://github.com/agunggema-debug/agri-dashboard.git", // Sesuaikan dengan nama repository GitHub
   },
 });
